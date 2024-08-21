b073edf7126a4ebf4c68a89b1a7b5953b06d9dd2
Eduardo <eduardo25082002@gmail.com>
2024-08-20 21:28:59 -0300

    Funcoes com numero var

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
-def funcao():
-    variavel_local = 10
-    print(variavel_local)
-
-variavel_global = 20
-
-def funcao2():
-    print(variavel_global)
-
-funcao()
-funcao2()
+def soma_variavel(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total
+print(soma_variavel(1, 2, 3)) #Imprime 6
+print(soma_variavel(4, 5, 6, 7)) #Imprime 22
