96e8bc513e5156b6ecaf8171b53078b3c369c9ad
Eduardo <eduardo25082002@gmail.com>
2024-08-26 20:43:13 -0300

    Leitura e Escritura de Arquivos

--- Dados.py
+++ Dados.py
@@ -19,3 +19,24 @@ else:
 nome = "Eduardo"
 idade = 22
 print(f"Olá, meu nome é {nome} e tenho {idade} anos. ")
+
+# Leitura e Escrita de Arquivos
+
+# Leitura com 'r'  
+arquivos = open("dados.txt", "r")
+conteudo = arquivos.read()
+print(conteudo)
+arquivos.close()
+
+
+
+# Escrita com'w'
+arquivo = open("dados.txt", "w")
+arquivo.write("Olá mundo !")
+arquivo.close()
+
+
+# Abertura e fechamento
+with open("dados.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
