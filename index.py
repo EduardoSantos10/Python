1fe56cebcd9bf94021eecedf418407a985f201ec
Eduardo <eduardo25082002@gmail.com>
2024-08-26 15:22:49 -0300

    Anotações 1

--- index.py
+++ index.py
@@ -1,7 +1,46 @@
-def soma_variavel(*numeros):
-    total = 0
-    for numero in numeros:
-        total += numero
-    return total
-print(soma_variavel(1, 2, 3)) #Imprime 6
-print(soma_variavel(4, 5, 6, 7)) #Imprime 22
+print("Olá, mundo")
+"""
+#numeros inteiros
+idade = 22
+quantidade = 200
+
+#float
+preco = 9.99
+altura = 1.75
+
+#string
+nome = "Eduardo"
+mensagem = 'Salve, Irmão'
+
+#booleanos
+é maior de idade = True
+tem desconto = False
+"""
+#Estrutura Condicionais
+
+#IF
+idade = 18
+
+if idade >= 18:
+    print("Você é maior de idade")
+
+
+#IF-ELSE
+idade = 15
+
+if idade >= 18:
+    print("Maior de Idade")
+else:
+    print("Menor de Idade")
+
+#IF-ELSE-ELIF
+nota = 10
+
+if nota >= 9:
+    print("Excelente")
+elif nota >= 8:
+    print("Muito Bom")
+elif nota >= 7:
+    print("Bom")
+else:
+    print("Precisa Melhorar")
