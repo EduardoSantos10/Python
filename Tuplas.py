2798a91b8e2d8c66a126390e19be65c5eae199f6
Eduardo <eduardo25082002@gmail.com>
2024-08-26 15:52:35 -0300

    Tuplas

---
+++ Tuplas.py
@@ -0,0 +1,17 @@
+#Criação e Acesso
+ponto = (10, 20)
+
+print(ponto[0])
+print(ponto[1])
+
+#Métodos de Tuplas
+minha_tupla = (1, 2, 3, 2, 4, 2)
+
+#count
+print (minha_tupla.count(2))
+
+#index
+print (minha_tupla.index(2, 2))
+
+#len
+print (minha_tupla.len(1, 2, 3, 2, 4, 2))
